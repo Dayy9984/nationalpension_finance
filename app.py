b0d7c0e2c2115d35c7f7388ad731ef4bcab0e6bb
Dayy9984 <87189372+Dayy9984@users.noreply.github.com>
2023-05-21 18:36:05 +0900

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import concurrent.futures
 import streamlit as st
 from mpl_finance import candlestick2_ohlc
 
-st.title(':blue[국민연금] ')
+st.title('      :blue[국민연금] ')
 st.title('2020/2021년도 투자종목 분석:page_with_curl:')
 st.title('')
 font_path = 'NanumBarunGothicLight.ttf'
